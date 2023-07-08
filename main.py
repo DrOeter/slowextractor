3fbf02936a340d6b074835c99a934d5b48504cc0
Moritz Mögel <moegelmoritz@gmail.com>
2023-07-08 16:37:03 +0200

    new file:   .gitignore 	new file:   checklist.txt 	modified:   main.py

--- main.py
+++ main.py
@@ -10,20 +10,36 @@ byteSize = int(sys.argv[4])
 
 fromList = glob.glob(fromPath + '/*')
 
-for fromfilePath in fromList:
-    tofilePath = toPath + fromfilePath[fromfilePath.rfind('/'):]
-    print(tofilePath)
+with open( 'checklist.txt' , 'r+') as file:
+    checkList = file.readlines()
+    try:        
+        for item in checkList:
+            fromList.remove(item[:-1])
+    except:
+        pass
 
-    with open(fromfilePath, 'rb') as fromFile:
-        with open( tofilePath , 'wb+') as toFile:
-            fileSize = os.path.getsize(fromfilePath)
+    for fromfilePath in fromList:
+        tofilePath = toPath + fromfilePath[fromfilePath.rfind('/'):]
+        print(tofilePath)
+        sys.stdout.flush()
 
-            while fileSize >= byteSize:
-                toFile.write( fromFile.read(byteSize) )
-                print(fileSize)
+        try:        
+            with open(fromfilePath, 'rb') as fromFile:
+                with open( tofilePath , 'wb+') as toFile:
+                    fileSize = os.path.getsize(fromfilePath)
 
-                time.sleep(sleepTime)
+                    while fileSize >= byteSize:
+                        toFile.write( fromFile.read(byteSize) )
+                        print(fileSize)
+                        sys.stdout.flush()
 
-                fileSize -= byteSize
-            
-            toFile.write( fromFile.read(fileSize) )
+                        time.sleep(sleepTime)
+
+                        fileSize -= byteSize
+                    
+                    toFile.write( fromFile.read(fileSize) )
+        except Exception as e:
+            print(e)
+            sys.stdout.flush()
+        file.write(fromfilePath + '\n')
+        file.flush()
